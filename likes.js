1bbc0a1491d00fa43677ffe433b80297815361d6
JonahEspinosa <103456315+JonahEspinosa@users.noreply.github.com>
2022-09-19 11:15:28 -0400

    Add files via upload

---
+++ likes.js
@@ -0,0 +1,40 @@
+const CountryPTag = document.querySelector('#Country')
+const animePTag = document.querySelector('#Countrys')
+const reloadButton = document.querySelector('#reload-Country-button')
+const likeButton = document.querySelector('#like-button')
+const likedCountrysContainer = document.querySelector('#liked-Country')
+
+let currentCountry = {}
+let likedCountrys = []
+
+function saveCountry() {
+  if (!likedCountrys.includes(currentCountry)) {
+    likedCountrys.push(currentCountry)
+    
+
+    function removeCountry(event) {
+      event.target.remove()
+      const removeCountrys = likedCountrys.filter(likedCountry => {
+        return likedCountry !== currentCountry
+      }
+        )
+        likedCountrys = removeCountrys
+    } 
+
+
+    const li = document.createElement('li')
+    li.innerText = currentCountry.Country
+    likedCountrysContainer.append(li)
+    li.addEventListener('click', event => removeCountry(event))
+  } else {
+    alert('Opps! You already have that country!')
+  }
+}
+
+
+
+reloadButton.addEventListener('click', getCountry)
+
+getAnimeCountry()
+
+likeButton.addEventListener('click', saveCountry)
